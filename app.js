e330ccabaaf1d732b45a7123449ea98ea5a69345
Gia <142356893+Gia-11@users.noreply.github.com>
2024-08-27 19:11:46 -0400

    shows hides views

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
+// hides display view page
 function handleEditProfile() {
     document.getElementById("edit-view").style.display = "block"
     document.getElementById("display-view").style.display = "none"
 }
+
+// hides edit view page
+function handleEditProfile() {
+    document.getElementById("display-view").style.display = "block"
+    document.getElementById("edit-view").style.display = "none"
+}
+
+
