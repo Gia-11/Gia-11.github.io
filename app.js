dc287097df70ee397bec5eff11669d47050cb44d
Gia <142356893+Gia-11@users.noreply.github.com>
2024-09-01 08:05:40 -0400

    Added variable for updated name, email, interests

--- app.js
+++ app.js
@@ -9,7 +9,15 @@ function handleUpdateProfile() {
     var updatedName = document.getElementById("input-name").value
     var name = document.getElementById("name")
     name.textContent = updatedName
-    
+
+    var updatedEmail = document.getElementById("input-email").value
+    var email = document.getElementById("email")
+    email.textContent = updatedEmail
+
+    var updatedInterests = document.getElementById("input-interests").value
+    var interests = document.getElementById("interests")
+    interests.textContent = updatedInterests
+
     document.getElementById("edit-view").style.display = "none"
     document.getElementById("display-view").style.display = "block"
 }
